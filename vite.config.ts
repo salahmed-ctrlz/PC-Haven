a90d0f2caee6f4793b655a5aba88dd2c2d10fc14
salahmed-ctrlz <medkoursalaheddine@gmail.com>
2025-03-13 19:53:00 +0100

    Updated main.tsx, vite.config.ts, and package files

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/PC-Haven/", 
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  base: "", 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"), // Ensure this points to the `src` directory
     },
   },
-}));
+  plugins: [react()],
+});
